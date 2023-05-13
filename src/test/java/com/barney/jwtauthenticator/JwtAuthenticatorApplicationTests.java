51cc13c8a3a3f3ecd6927ec639a6a528b929acd4
betelgeusexru <betelgeusexru@gmail.com>
2023-05-14 00:05:00 +0500

    init

---
+++ src/test/java/com/barney/jwtauthenticator/JwtAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barney.jwtauthenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
